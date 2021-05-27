6466a172c90d7a91b0cc6c2eabeddeb2332a269d
Oishi Poddar <oishi.poddar@gmail.com>
2021-05-27 21:38:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def go(config: DictConfig):
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
+                version = "master",
                 parameters={
                     "sample": config["etl"]["sample"],
                     "artifact_name": "sample.csv",
